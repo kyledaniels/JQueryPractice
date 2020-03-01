b94368511a3d845ecee7b5496fd98aa63f97918b
kyle67 <daniels.kyle@gmail.com>
2020-03-01 10:32:53 -0500

    added buttons to page

--- index.js
+++ index.js
@@ -20,3 +20,10 @@ $("h1").on("click", function(){
     $("h1").css("color", "purple");
 })
 
+$("h1").before("<button>New</button>");
+
+$("h1").after("<button>New</button>");
+
+$("h1").prepend("<button>Before</button>");
+
+$("h1").append("<button>After</button>");
